5e90eb56792e7d942073b41560e604949e9c6f0d
AntKov <anton.beskyd@gmail.com>
2023-12-04 21:02:01 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,2 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-def func2(x):
-    return x + 2
-
-def test_answer():
-    assert func(2) == 3
-
-def test_answer2():
-    assert func2(2) == 4
+def add(a,b):
+    return a+b
